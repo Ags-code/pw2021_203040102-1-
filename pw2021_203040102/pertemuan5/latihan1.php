0d1e1de86fe0cbcf9ddd2060f7f1ae9deee0f380
Ags-code <agungseptiana02@gmail.com>
2021-03-28 15:13:51 +0700

    menambahkan file

---
+++ pw2021_203040102/pertemuan5/latihan1.php
@@ -0,0 +1,30 @@
+<?php
+// array 
+// Variabel yang dapat memiliki banyak nilai
+
+// membuat array
+// cara lama
+$hari = array("Senin", "Selasa", "Rabu");
+// cara baru
+$bulan = ["Januari", "Februari", "Maret"];
+$arr1 = [123, "tulisam", false];
+
+// Menampilakn Array
+// var_dump() / print_r()
+// var_dump($hari);
+// echo "<br>";
+// print_r($bulan);
+// echo "<br>";
+
+// Menampilkan 1 elemen pada array
+// echo $arr1[0];
+// echo "<br>";
+// echo $bulan[1]
+
+// Menambahkan elemen baru pada array 
+var_dump($hari);
+$hari[] = "Kamis";
+$hari[] = "Jum'at";
+echo "<br>";
+var_dump($hari);
+?>
---
+++ pw2021_203040102/pertemuan5/latihan3.php
@@ -0,0 +1,48 @@
+<?php
+// array assosiaive
+// definisinya sama seperti array numerik, kecuali
+// key-nya adalah string yang kita bua sendiri
+
+$mahasiswa = [
+    [
+        "nama" => "Agung Septiana",
+        "nrp" => "203040102",
+        "email" => "agungseptiana02@gmail.com",
+        "jurusan" => "Teknik Informatika",
+        "gambar" => "ell.jpg"
+    ],
+    [
+        "nama" => "Yukinoshita Yukino",
+        "nrp" => "203040103",
+        "email" => "yukinoshitayukino02@gmail.com",
+        "jurusan" => "Teknik Informatika",
+        "gambar" => "yukino.png"
+    ],
+]
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Latihan 3</title>
+    <style>
+        img {
+            max-width: 120px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Daftar Mahasiswa</h1>
+
+    <?php foreach( $mahasiswa as $mhs) : ?>
+        <ul>
+            <li>
+                <img src="img/<?= $mhs["gambar"]; ?>">
+            </li>
+            <li>Nama : <?= $mhs["nama"]; ?></li>
+            <li>NRP : <?= $mhs["nrp"]; ?></li>
+            <li>Jurusan : <?= $mhs["jurusan"]; ?></li>
+            <li>Email : <?= $mhs["email"]; ?></li>
+        </ul>
+    <?php endforeach; ?>
+</body>
+</html>
